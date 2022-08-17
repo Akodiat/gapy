a6bd9ea733d9b7706ead2a1581872044491383d8
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2022-08-17 01:41:36 -0700

    Fix elitism

--- geneticAlgoritm.py
+++ geneticAlgoritm.py
@@ -6,6 +6,7 @@ class GeneticAlgorithm:
         self.population = population
         self.fitnessFunc = fitnessFunc
         self.fitnessTarget = fitnessTarget
+        self.allTimeBestFitness = 0
 
     def stepGeneration(self, nProcesses=1):
         if nProcesses > 1:
@@ -15,6 +16,10 @@ class GeneticAlgorithm:
             fitnesses = [self.fitnessFunc(i) for i in self.population]
         maxFitness = max(fitnesses)
         best = self.population[fitnesses.index(maxFitness)]
+        if maxFitness > self.allTimeBestFitness:
+            print("Found new all-time best")
+            self.allTimeBestFitness = maxFitness
+            self.allTimeBest = best
 
         if self.fitnessTarget and maxFitness >= self.fitnessTarget:
             print("Reached target fitness: {}".format(maxFitness))
@@ -27,9 +32,10 @@ class GeneticAlgorithm:
                 weights=fitnesses,
                 k=len(self.population) - 1
             )]
-            self.population.append(best) # Elitism
         for g in self.population:
             g.mutate()
+
+        self.population.append(self.allTimeBest) # Elitism
 
         return maxFitness, best
 
